c6c083e03d8047f59d82088bd6ab14c3095f5098
Tim Benjamin <tim@tienjin.com>
2023-12-04 13:03:56 +0000

    done day 3

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -20,18 +20,9 @@ func checkForSymbol(snip []string) bool {
 	return false
 }
 
-func checkForAsterisk(snip []string) bool {
-	for _, c := range snip {
-		if c == "*" {
-			return true
-		}
-	}
-	return false
-}
-
 func partOne() int {
 	total := 0
-	re := regexp.MustCompile("[0-9]+")
+	re := regexp.MustCompile(`[0-9]+`)
 	for lineNum, line := range input {
 		numberLocations := re.FindAllStringSubmatchIndex(line, -1)
 		for _, location := range numberLocations {
@@ -40,23 +31,20 @@ func partOne() int {
 			var charsToCheck []string
 			from := location[0]
 			to := location[1]
-			fmt.Printf("number %v is from: %v to: %v\n", number, from, to)
+			// fmt.Printf("number %v is from: %v to: %v\n", number, from, to)
 
 			// need to get any chars either side of it:
 			if from > 0 {
 				from--
 				charsToCheck = append(charsToCheck, line[from:from+1])
-				fmt.Printf("char before: %v\n", line[from:from+1])
 			}
-			fmt.Printf("len of line: %v\n", len(line))
 			if to < len(line) {
 				to++
 				charsToCheck = append(charsToCheck, line[to-1:to])
-				fmt.Printf("char after: %v\n", line[to-1:to])
 			}
 
 			// the expanded snippet horizontal positions
-			fmt.Printf("expanded: from: %v / to: %v\n", from, to)
+			// fmt.Printf("expanded: from: %v / to: %v\n", from, to)
 
 			// check line above
 			if lineNum > 0 {
@@ -82,13 +70,60 @@ func partOne() int {
 	return total
 }
 
+func getPartNumbersAt(lineNum int, position int) []int {
+	re := regexp.MustCompile(`[0-9]+`)
+	numberLocations := re.FindAllStringSubmatchIndex(input[lineNum], -1)
+	partNumbers := []int{}
+	for _, loc := range numberLocations {
+		if position >= loc[0]-1 && position <= loc[1] {
+			partNumber, _ := strconv.Atoi(input[lineNum][loc[0]:loc[1]])
+			partNumbers = append(partNumbers, partNumber)
+		}
+	}
+	return partNumbers
+}
+
 func partTwo() int {
 	total := 0
 
-	// find pairs of numbers that are adjacent to the same * character
-
-	// find *'s and then find numbers that are adjacent to them?
-
+	// now find all asterisks and see if exactly two numbers are adjacent
+	re := regexp.MustCompile(`[0-9]+`)
+	for lineNum, line := range input {
+		for pos, c := range line {
+			if c == '*' {
+				from, to := pos, pos
+				var partNumbersFound []int
+				if pos > 0 {
+					if re.MatchString(string(line[pos-1])) {
+						partNumbersFound = append(partNumbersFound, getPartNumbersAt(lineNum, pos-1)...)
+					}
+					from--
+				}
+				if pos < len(line)-1 {
+					if re.MatchString(string(line[pos+1])) {
+						partNumbersFound = append(partNumbersFound, getPartNumbersAt(lineNum, pos+1)...)
+					}
+					to++
+				}
+				if lineNum > 0 {
+					if re.MatchString(input[lineNum-1][from : to+1]) {
+						partNumbersFound = append(partNumbersFound, getPartNumbersAt(lineNum-1, pos)...)
+					}
+				}
+				if lineNum < len(input)-1 {
+					if re.MatchString(input[lineNum+1][from : to+1]) {
+						partNumbersFound = append(partNumbersFound, getPartNumbersAt(lineNum+1, pos)...)
+					}
+				}
+				if len(partNumbersFound) == 2 {
+					// fmt.Printf("found a gear at pos: %v in line: %v\n", pos, lineNum)
+					// fmt.Printf("part numbers: %v\n", partNumbersFound)
+					// fmt.Printf("ratio: %v\n", partNumbersFound[0]*partNumbersFound[1])
+					total += (partNumbersFound[0] * partNumbersFound[1])
+				}
+			}
+		}
+	}
 	return total
 }
 
